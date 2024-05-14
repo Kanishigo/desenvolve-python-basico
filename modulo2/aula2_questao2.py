6bb1767510a5aa867096c7cd2f30830febf51341
Robson Kawanishi <165382695+Kanishigo@users.noreply.github.com>
2024-05-13 21:42:55 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,8 @@
+aux = "O resultado é"
+x = 10
+y = 3.5
+r = x + y
+print (aux, type (aux))
+print (x, type(x))
+print (y, type(y))
+print (r, type(r))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,5 @@
+saldo = 500.00
+saldo = saldo * 1.01
+saldo = saldo * 1.01
+saldo = saldo * 1.01
+print ("Após 3 meses meu saldo é:", saldo)
